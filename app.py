ef74e213bfd3701276804bf0202cf7f1362bdf6b
Adejolaoluwa <84287904+JollyJola@users.noreply.github.com>
2023-01-05 20:24:31 +0100

    Quite a lot of changes

--- app.py
+++ app.py
@@ -10,15 +10,21 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home(): 
     return render_template("index.html")
 
+@app.route('/predict/')
+def transaction(): 
+    return render_template("transaction.html")
 
-@app.route('/detect', methods=['POST'])
+@app.route('/transaction/', methods=['POST'])
 def predict(): 
     fields = [float(x) for x in request.form.values()]
     fieldArray = [np.array(fields)]
     prediction = model.predict(fieldArray)
 
     if prediction == 0:
-        return(render_template('/predict', prediction_text= "This transaction is not fraudulent"))
+        return(render_template('transaction.html', prediction_text= "This transaction is not fraudulent"))
     else: 
-        return(render_template('/predict', prediction_text= "This transaction is fraudulent"))
+        return(render_template('transaction.html', prediction_text= "This transaction is fraudulent"))
 
+
+if __name__ == "__main__":
+    app.run()
